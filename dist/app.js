f640af635876b25807b73cf8ae5762494c37990e
meanjula <anjula.ghale@gmail.com>
2022-02-18 11:14:04 +0200

    added functionality for adding and removing task

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,34 @@ const addButton = document.querySelector(".add-item");
 const listGroup = document.querySelector(".list-group");
 const clearButton = document.querySelector("#clear");
 const completeButton = document.querySelector(".complete-btn");
-//# sourceMappingURL=app.js.map
+function addList() {
+    listGroup.insertAdjacentHTML("beforeend", `<li class="newlist"><p>${input.value}</P> 
+		<button type="button" class="trash-btn"><i class="fa fa-trash"></i></button>
+		<button type="button" class="complete-btn"><i class="fa fa-check"></i></button>
+	  </li>`);
+    input.value = ""; //clears input field after
+}
+addButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (input.value != "") {
+        addList();
+        const listItems = document.querySelectorAll("li");
+        listItems.forEach((item) => {
+            if (item) {
+                item.addEventListener("click", (e) => {
+                    const clickedbtn = e.target;
+                    if (clickedbtn.className === "trash-btn") {
+                        item.remove();
+                    }
+                    else if (clickedbtn.className === "complete-btn") {
+                        item.classList.toggle("completed");
+                    }
+                });
+            }
+        });
+    }
+});
+clearButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    listGroup.innerHTML = "";
+});
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,43 @@
 "use strict";
+
 const input = document.querySelector(".new-list") as HTMLInputElement;
 const addButton = document.querySelector(".add-item") as HTMLButtonElement;
 const listGroup = document.querySelector(".list-group") as HTMLOListElement;
 const clearButton = document.querySelector("#clear") as HTMLButtonElement;
 const completeButton = document.querySelector(".complete-btn")as HTMLButtonElement;
+
+function addList() {
+	listGroup.insertAdjacentHTML(
+	  "beforeend",
+	  `<li class="newlist"><p>${input.value}</P> 
+		<button type="button" class="trash-btn"><i class="fa fa-trash"></i></button>
+		<button type="button" class="complete-btn"><i class="fa fa-check"></i></button>
+	  </li>`
+	);
+	input.value = ""; //clears input field after
+  }
+  
+  addButton.addEventListener("click", (e:Event) => {
+	e.preventDefault();
+	if (input.value != "") {
+	  addList();
+	  const listItems = document.querySelectorAll("li");
+	  listItems.forEach((item) => {
+		if (item ) {
+		  item.addEventListener("click", (e:Event) => {
+		 	const clickedbtn = <HTMLButtonElement>e.target;
+		 	if (clickedbtn.className === "trash-btn") {
+			  item.remove();
+			} else if (clickedbtn.className === "complete-btn") {
+			  item.classList.toggle("completed");
+			}
+		   });
+		 }
+	  });
+	}
+  });
+  clearButton.addEventListener("click", (e:Event) => {
+	  e.preventDefault();
+	listGroup.innerHTML = "";
+  });
+  
